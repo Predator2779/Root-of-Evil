3ec8c922e7012349b1e9fd3822409a353a730b79
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-03-04 20:06:20 +0500

    added middle enemy animations

--- Assets/Scripts/Character/ValueStorages/Health.cs
+++ Assets/Scripts/Character/ValueStorages/Health.cs
@@ -2,14 +2,44 @@
 
 namespace Character.ValueStorages
 {
-    public class Health : ValueStorage
+    public class Health
     {
-        public Health(float maxValue) : base(maxValue)
+        public Health(float maxValue)
         {
+            MaxValue = maxValue;
         }
 
-        public Health(float maxValue, ValueBar bar) : base(maxValue, bar)
+        public Health(float maxValue, ValueBar bar)
         {
+            MaxValue = maxValue;
+            Bar = bar;
         }
+        
+        protected ValueBar Bar { get; }
+        public float MinValue { get; } = 0;
+        public float CurrentValue { get; private set; }
+        public float MaxValue { get; }
+
+        public virtual void Increase(float value)
+        {
+            float newValue = CurrentValue + value;
+            SetValue(newValue > MaxValue ? MaxValue : newValue);
+            ChangeBar();
+        }
+
+        public virtual void Decrease(float value)
+        {
+            float newValue = CurrentValue - value;
+            SetValue(newValue < MinValue ? MinValue : newValue);
+            ChangeBar();
+        }
+
+        private void ChangeBar()
+        {
+            if (Bar != null) Bar.SetCurrentValue(GetPercentageRation());
+        }
+
+        private void SetValue(float value) => CurrentValue = value;
+        private float GetPercentageRation() => CurrentValue / MaxValue * 100;
     }
 }
