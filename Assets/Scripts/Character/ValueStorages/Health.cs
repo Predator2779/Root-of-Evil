b80407ea6f58ba252cf77a1177684abf75cf3fef
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-03-04 18:26:12 +0500

    added test-scene, healths scripts and bar prefab

---
+++ Assets/Scripts/Character/ValueStorages/Health.cs
@@ -0,0 +1,15 @@
+﻿using Character.ValueStorages.Bars;
+
+namespace Character.ValueStorages
+{
+    public class Health : ValueStorage
+    {
+        public Health(float maxValue) : base(maxValue)
+        {
+        }
+
+        public Health(float maxValue, ValueBar bar) : base(maxValue, bar)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Character/ValueStorages/ValueStorage.cs
@@ -0,0 +1,46 @@
+﻿using Character.ValueStorages.Bars;
+
+namespace Character.ValueStorages
+{
+    public abstract class ValueStorage
+    {
+        public ValueStorage(float maxValue)
+        {
+            MaxValue = maxValue;
+        }
+
+        public ValueStorage(float maxValue, ValueBar bar)
+        {
+            MaxValue = maxValue;
+            Bar = bar;
+        }
+        
+        protected ValueBar Bar { get; }
+        public float MinValue { get; } = 0;
+        public float CurrentValue { get; private set; }
+        public float MaxValue { get; }
+
+        public virtual void Increase(float value)
+        {
+            float newValue = CurrentValue + value;
+            SetValue(newValue > MaxValue ? MaxValue : newValue);
+            ChangeBar();
+        }
+
+        public virtual void Decrease(float value)
+        {
+            float newValue = CurrentValue - value;
+            SetValue(newValue < MinValue ? MinValue : newValue);
+            ChangeBar();
+        }
+
+        public void SetValue(float value) => CurrentValue = value;
+
+        private void ChangeBar()
+        {
+            if (Bar != null) Bar.SetCurrentValue(GetPercentageRation());
+        }
+
+        private float GetPercentageRation() => CurrentValue / MaxValue * 100;
+    }
+}
